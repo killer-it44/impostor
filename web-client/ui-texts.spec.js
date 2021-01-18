d8c438ac139cf718e05ea913f90c50d4d59e5695
xiaoye <keller_jens@gmx.de>
2021-01-18 13:19:02 +0800

    cover missing special cases in tests

--- web-client/ui-texts.spec.js
+++ web-client/ui-texts.spec.js
@@ -27,5 +27,40 @@ describe('UI Texts', () => {
         let uiTexts = new UiTexts({ 'en': { 'MY_TEXT': 'hello world' } })
         expect(uiTexts.loadMergedTexts([])['MY_TEXT']).toBe('hello world')
     })
+
+    it('defaults to the general language if a specific language is requested but not available', () => {
+        let uiTexts = new UiTexts({ 
+            'en': { 'MY_TEXT': 'hello world' },
+            'de': { 'MY_TEXT': 'hallo welt' } 
+        })
+        expect(uiTexts.loadMergedTexts(['de-DE'])['MY_TEXT']).toBe('hallo welt')
+    })
+
+    it('will not default if general language is already specified, and keep the order', () => {
+        let uiTexts = new UiTexts({ 
+            'en': { 'MY_TEXT': 'hello world' },
+            'de': { 'MY_TEXT': 'hallo welt' },
+            'de-CH': { 'MY_TEXT': 'grüezi welt' } 
+        })
+        expect(uiTexts.loadMergedTexts(['de', 'de-CH'])['MY_TEXT']).toBe('hallo welt')
+    })
+
+    it('defaults to first found more specific language if a general language is requested but not available', () => {
+        let uiTexts = new UiTexts({ 
+            'en': { 'MY_TEXT': 'hello world' },
+            'de-CH': { 'MY_TEXT': 'grüezi welt' },
+            'de-DE': { 'MY_TEXT': 'hallo welt' }
+        })
+        expect(uiTexts.loadMergedTexts(['de'])['MY_TEXT']).toBe('grüezi welt')
+    })
+
+    it('will not overwrite a general language with a more specific language', () => {
+        let uiTexts = new UiTexts({ 
+            'en': { 'MY_TEXT': 'hello world' },
+            'de': { 'MY_TEXT': 'hallo welt' },
+            'de-CH': { 'MY_TEXT': 'grüezi welt' }
+        })
+        expect(uiTexts.loadMergedTexts(['de'])['MY_TEXT']).toBe('hallo welt')
+    })
 })
 
