40165f641a958dfafd387c93523b119e3e849d61
xiaoye <keller_jens@gmx.de>
2021-01-18 15:43:44 +0800

    get rid of constructor logic

--- web-client/ui-texts.js
+++ web-client/ui-texts.js
@@ -1,14 +1,6 @@
 'use strict'
 
 const UiTexts = function (availableTexts) {
-    // REVISE can it be done as part of the lookup to eliminate constructor logic?
-    // fallbacks: if e.g. there is no 'en.json', but 'en-US.json', then requesting texts for 'en' should return en-US
-    Object.keys(availableTexts).filter(lang => lang.length > 2).forEach(specificLang => {
-        if (!availableTexts[specificLang.substr(0, 2)]) {
-            availableTexts[specificLang.substr(0, 2)] = availableTexts[specificLang]
-        }
-    })
-
     const addGeneralLanguagesIfNotExisting = function (languages) {
         const specificLangs = languages.filter(l => l.length > 2)
         specificLangs.forEach((sl) => {
@@ -22,7 +14,11 @@ const UiTexts = function (availableTexts) {
     const mergeTextsForLanguages = function (languages) {
         let mergedTexts = {}
         languages.forEach((language) => {
-            mergedTexts = { ...availableTexts[language] || {}, ...mergedTexts }
+            const moreSpecificLang = Object.keys(availableTexts).find(lang => lang.substr(0, 2) === language)
+            mergedTexts = {
+                ...(availableTexts[language] || availableTexts[moreSpecificLang] || {}),
+                ...mergedTexts
+            }
         })
         return mergedTexts
     }
